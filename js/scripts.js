b9e74f2ab87850f7d222cd3309f5c41c56ddfd0b
BirrellC <vaiezmedia@gmail.com>
2021-04-24 11:41:31 +0100

    Fix: updated animation name

--- js/scripts.js
+++ js/scripts.js
@@ -45,14 +45,9 @@ window.onscroll = function () {
 
 AOS.init({
     // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    disable: 'phone',
+    startEvent: 'DOMContentLoaded',
+    initClassName: 'aos-init',
 
     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
     offset: 120, // offset (in px) from the original trigger point
